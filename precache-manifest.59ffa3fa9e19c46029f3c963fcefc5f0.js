f0ed21b060b3179eeaba7924d2ffd647605730ef
abhijeetsinghtomar <abhijeetsinghemail@gmail.com>
2019-12-11 23:44:45 +0530

    Updates

---
+++ precache-manifest.59ffa3fa9e19c46029f3c963fcefc5f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1946ff7052333fe1079b3ee31f68b62",
+    "url": "/react-contacts/index.html"
+  },
+  {
+    "revision": "51b5c03c1603338dff34",
+    "url": "/react-contacts/static/js/2.4e91b527.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-contacts/static/js/2.4e91b527.chunk.js.LICENSE"
+  },
+  {
+    "revision": "feed1a777d8ea455c20b",
+    "url": "/react-contacts/static/js/main.85efc612.chunk.js"
+  },
+  {
+    "revision": "75edaa13739f47603070",
+    "url": "/react-contacts/static/js/runtime-main.bf25f89c.js"
+  }
+]);
